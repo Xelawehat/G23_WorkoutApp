3e764865c8e8ee143b6b156e25f2fed3bd0105b5
nwassom <newassom@gmail.com>
2024-04-21 16:40:31 -0400

    Remove node_modules from repository

--- node_modules/react-native-date-picker/android/src/main/java/com/henninghall/date_picker/ui/SpinnerState.java
+++
@@ -1,6 +0,0 @@
-package com.henninghall.date_picker.ui;
-
-public enum SpinnerState {
-    idle,
-    spinning
-}
--- node_modules/react-native-date-picker/android/src/main/java/com/henninghall/date_picker/ui/SpinnerStateListener.java
+++
@@ -1,5 +0,0 @@
-package com.henninghall.date_picker.ui;
-
-public interface SpinnerStateListener {
-    void onChange(SpinnerState event);
-}
