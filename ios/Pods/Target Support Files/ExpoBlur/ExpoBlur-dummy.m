7f306977359f1dfe4dc12ba2c562bc6d58a6c2f7
nwassom <newassom@gmail.com>
2024-04-21 16:37:01 -0400

    Updated .gitignore and removed iOS build files from Git index

--- ios/Pods/Target Support Files/ExpoBlur/ExpoBlur-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ExpoBlur : NSObject
-@end
-@implementation PodsDummy_ExpoBlur
-@end
--- ios/Pods/Target Support Files/Pods-WorkoutApp/Pods-WorkoutApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WorkoutApp : NSObject
-@end
-@implementation PodsDummy_Pods_WorkoutApp
-@end
